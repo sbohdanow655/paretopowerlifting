fd09e2d5d2635c812e5a67e96768b1832ac41997
Scott Bohdanow <scottbohdanow@Scotts-MacBook-Air.local>
2021-08-29 09:29:22 -0400

    redid exercise prescription

--- lib/classes/ExercisePrescription.dart
+++ lib/classes/ExercisePrescription.dart
@@ -1,28 +1,21 @@
-abstract class ExercisePrescription {
+import '../assets/constants.dart';
+
+class ExercisePrescription {
   int _id;
   String _exercise;
-  int _weight;
+  double _weight;
   String _weightUnit;
   int _numSets;
   int _numReps;
+  int _phaseNumber;
 
-  ExercisePrescription(id, exercise, weight, weightUnit, numSets, numReps) {
-    _id = id;
-    _exercise = exercise;
-    _weight = weight;
-    _weightUnit = weightUnit;
-    _numSets = _numSets;
-    _numReps = numReps;
-  }
+  ExercisePrescription(this._id, this._exercise, this._weight, this._weightUnit, this._numSets, this._numReps, this._phaseNumber);
 
   int getId() {
     return _id;
   }
 
-  void pass();
-  void fail();
-
   String toString() {
-    return _exercise + " " + _weight.toString() + _weightUnit + " " + _numSets + "x" + _numReps;
+    return _exercise + " " + _weight.toString() + _weightUnit + " " + _numSets.toString() + "x" + _numReps.toString();
   }
 }
--- lib/classes/ExercisePrescriptions/SquatPrescription.dart
+++ lib/classes/ExercisePrescriptions/SquatPrescription.dart
@@ -1,25 +1,27 @@
-import 'assets/constants.dart';
-import '../ExcercisePrescription.dart';
+import '../../assets/constants.dart';
+import '../ExercisePrescription.dart';
+import 'dart:collection';
 
 class SquatPrescription extends ExercisePrescription {
-  
-  SquatPrescription(weight, weightUnit, numSets, numReps) {
-    super(Constants.exerciseSquat, weight, weightUnit, numSets, numReps);
-  }
-  
-  @override
-  void pass() {
-    if (_weightUnit == Constants.weightUnitLbs) {
-      _weight += Constants.bigWeightIncrementLbs;
-    } else {
-      _weight += Constants.bigWeightIncrementKg;
-    }
-}
 
-  @override
-  void fail() {
-    if (_phaseNumber < 3) {
-      _phaseNumber++;
+  HashMap<int, double> weightAddMap;
+  HashMap<int, int> setMap;
+  HashMap<int, int> repMap;
+
+
+  SquatPrescription(id, exercise, weight, weightUnit, numSets, numReps, phaseNumber) :
+    super(id, exercise, weight, weightUnit, numSets, numReps, phaseNumber) {
+      if (weightUnit == Constants.weightUnitLbs) {
+        weightAddMap[1] = Constants.bigWeightIncrementLbs;
+        weightAddMap[2] = Constants.bigWeightIncrementLbs;
+        weightAddMap[3] = Constants.bigWeightIncrementLbs;
+      } else {
+        weightAddMap[1] = Constants.bigWeightIncrementKg;
+        weightAddMap[2] = Constants.bigWeightIncrementKg;
+        weightAddMap[3] = Constants.bigWeightIncrementKg;
+      }
+
+      s
     }
-  }
+    
 }
